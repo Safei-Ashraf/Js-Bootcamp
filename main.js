8d90573cd35254bbf40a89584dc4e808c7289e20
Safei Ashraf <safei.ashraf@gmail.com>
2022-09-07 16:57:38 +0200

    solutio: ex4

---
+++ main.js
@@ -0,0 +1,27 @@
+// console.log("Error");
+// console.table(["s", "x", "y", "z"]);
+// console.log(
+//   "%c------------------%c--------------",
+//   "color: red; font-size: 12px; font-weight: bold",
+//   "color: blue; font-size: 12px; font-weight: bold"
+// );
+// const body = document.getElementsByTagName("body");
+// const header = document.createElement("h1");
+// header.innerHTML = "YO YO";
+// header.setAttribute(
+//   "style",
+//   "color:blue; font-size: 80px; font-weight: bold; font-family: Arial; text-align: center;"
+// );
+// document.write(
+//   "<h1 style='color:blue; font-size: 80px; font-weight: bold; font-family: Arial; text-align: center;'> Hello world</h1>"
+// );
+
+// document.body.appendChild(header);
+
+//EX2:
+console.log(
+  "%cElzero %cWeb %cSchool",
+  "color: red; font-size:40px",
+  "color: green; font-size:40px; font-weight:bold",
+  "color: white; font-size:40px; background-color:blue"
+);

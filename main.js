e57a7dc84e45d19dbc7ce53db406b758f95352ee
Safei Ashraf <safei.ashraf@gmail.com>
2023-01-03 06:25:53 +0200

    feat: solving Numbers p1

--- main.js
+++ main.js
@@ -61,22 +61,40 @@
 // Replace ? With Arithmetic Operators
 // console.log(10 ? 20 ? 15 ? 3 ? 190 ? 10 ? 400); // 0 Failed
 
-let num = 3;
+// let num = 3;
 
-// Solution One
-console.log(num * num - num); // 6
+// // Solution One
+// console.log(num * num - num); // 6
 
-// Solution Two
-console.log(num + num); // 6
+// // Solution Two
+// console.log(num + num); // 6
 
-// Soultion Three
-console.log(((num*num*num )/ (num*num)) + num); // 6
+// // Soultion Three
+// console.log(((num*num*num )/ (num*num)) + num); // 6
 
-// Soultion Four
-console.log(num/num * num+num); // 6
+// // Soultion Four
+// console.log(num/num * num+num); // 6
 
-// Solution Five
-console.log(((num*num + num*num)/num)); // 6
+// // Solution Five
+// console.log(((num*num + num*num)/num)); // 6
 
-// Solution Six
-console.log((num/(num*num))* (num**num) - num); // 6
+// // Solution Six
+// console.log((num/(num*num))* (num**num) - num); // 6
+
+/*الدروس من رقم 023 إلى رقم 026 */
+// Examples
+// console.log(100_000); // 100000
+// console.log(100000); // 100000
+// console.log(5e4 + 5e4); // 100000
+
+// Your Solutions
+console.log(1e5); // 100000
+console.log(1_00_000); // 100000
+console.log(100000); // 100000
+console.log(parseInt("100000")); // 100000
+console.log(Number(100000.00)); // 100000
+console.log(10 *1e4); // 100000
+console.log(1_00*1_000); // 100000
+console.log(Math.pow(10,5)); // 100000
+console.log(Math.max(100000,10000,100,1)); // 100000
+console.log(Math.abs(-1e5)); // 100000

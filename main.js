5a5ae2282f08e755f5308a163c8ddc962eb879fd
Safei Ashraf <safei.ashraf@gmail.com>
2023-01-03 08:57:40 +0200

    feat: solving Conditionals P2

--- main.js
+++ main.js
@@ -187,7 +187,35 @@
 // // let num = 110; // "110"
 
 // console.log(num);
-let num = 190;
-if (num < 10) console.log("00" + num);
-if (num > 10 && num < 100) console.log("0" + num);
-if (num >= 100) console.log(num);
+// let num = 190;
+// if (num < 10) console.log("00" + num);
+// if (num > 10 && num < 100) console.log("0" + num);
+// if (num >= 100) console.log(num);
+
+//P2:
+let num1 = 9;
+let str = "9";
+let str2 = "20";
+
+// Output
+("{num1} Is The Same Value As {str}");
+("{num1} Is The Same Value As {str} But Not The Same Type");
+("{num1} Is Not The Same Value Or The Same Type As {str2}");
+("{str} Is The Same Type As {str2} But Not The Same Value");
+
+if (num1 == str) {
+	console.log(`${num1} Is The Same Value As ${str}
+`);
+}
+if (num1 == str && typeof num1 != typeof str) {
+	console.log(`${num1} Is The Same Value As ${str} But Not The Same Type
+`);
+}
+if (num1 != str && typeof num1 != typeof str2) {
+	console.log(`${num1} Is The Same Value As ${str2} But Not The Same Type
+`);
+}
+if (str2 !== str && typeof str == typeof str2) {
+	console.log(`${str} Is The Same Value As ${str2} But Not The Same Type
+`);
+}

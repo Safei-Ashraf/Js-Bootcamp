9a9808a821bba0695907ab8f9e18e775e65fe6bd
Safei Ashraf <safei.ashraf@gmail.com>
2023-01-03 09:05:25 +0200

    feat: solving Conditionals P4

--- main.js
+++ main.js
@@ -193,29 +193,100 @@
 // if (num >= 100) console.log(num);
 
 //P2:
-let num1 = 9;
-let str = "9";
-let str2 = "20";
+// let num1 = 9;
+// let str = "9";
+// let str2 = "20";
 
-// Output
-("{num1} Is The Same Value As {str}");
-("{num1} Is The Same Value As {str} But Not The Same Type");
-("{num1} Is Not The Same Value Or The Same Type As {str2}");
-("{str} Is The Same Type As {str2} But Not The Same Value");
+// // Output
+// ("{num1} Is The Same Value As {str}");
+// ("{num1} Is The Same Value As {str} But Not The Same Type");
+// ("{num1} Is Not The Same Value Or The Same Type As {str2}");
+// ("{str} Is The Same Type As {str2} But Not The Same Value");
 
-if (num1 == str) {
-	console.log(`${num1} Is The Same Value As ${str}
-`);
+// if (num1 == str) {
+// 	console.log(`${num1} Is The Same Value As ${str}
+// `);
+// }
+// if (num1 == str && typeof num1 != typeof str) {
+// 	console.log(`${num1} Is The Same Value As ${str} But Not The Same Type
+// `);
+// }
+// if (num1 != str && typeof num1 != typeof str2) {
+// 	console.log(`${num1} Is The Same Value As ${str2} But Not The Same Type
+// `);
+// }
+// if (str2 !== str && typeof str == typeof str2) {
+// 	console.log(`${str} Is The Same Value As ${str2} But Not The Same Type
+// `);
+// }
+
+//P4
+
+// Edit What You Want Here
+
+let num1 = 4;
+let num2 = 2;
+let num3 = 4;
+let num4 = 27;
+
+/*
+  Do Not Edit Below This Line
+  Needed Output
+  True 7 Times
+*/
+
+// Condition 1
+
+if (num1 > num2) {
+	console.log("True");
+} else {
+	console.log("False");
 }
-if (num1 == str && typeof num1 != typeof str) {
-	console.log(`${num1} Is The Same Value As ${str} But Not The Same Type
-`);
+
+// Condition 2
+
+if (num1 > num2 && num1 < num4) {
+	console.log("True");
+} else {
+	console.log("False");
 }
-if (num1 != str && typeof num1 != typeof str2) {
-	console.log(`${num1} Is The Same Value As ${str2} But Not The Same Type
-`);
+
+// Condition 3
+
+if (num1 > num2 && num1 === num3) {
+	console.log("True");
+} else {
+	console.log("False");
 }
-if (str2 !== str && typeof str == typeof str2) {
-	console.log(`${str} Is The Same Value As ${str2} But Not The Same Type
-`);
+
+// Condition 4
+
+if (num1 + num2 < num4) {
+	console.log("True");
+} else {
+	console.log("False");
+}
+
+// Condition 5
+
+if (num1 + num3 < num4) {
+	console.log("True");
+} else {
+	console.log("False");
+}
+
+// Condition 6
+
+if (num1 + num2 + num3 < num4) {
+	console.log("True");
+} else {
+	console.log("False");
+}
+
+// Condition 7
+
+if (num4 - (num1 + num3) + num2 === 21) {
+	console.log("True");
+} else {
+	console.log("False");
 }

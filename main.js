e1ce6ae529c1479493bf6e4f88b47b0e0ea490a8
Safei Ashraf <safei.ashraf@gmail.com>
2023-01-03 07:41:11 +0200

    feat: solving string challenge 2

--- main.js
+++ main.js
@@ -131,14 +131,25 @@
 // console.log(Math.floor(Math.random()*5)); // 0 || 1 || 2 || 3 || 4
 
 
-//الدروس من رقم 027 إلى رقم 030
+//String الدروس من رقم 027 إلى رقم 030
 
 //P1
 
-let userName = "Elzero";
-console.log(userName[0].toLowerCase().repeat(3)); // eee
-console.log(userName.charAt(0).toLowerCase()); // e
-console.log(userName.slice(0,1).toLowerCase()); // e
-console.log(userName.substring(0,1).toLowerCase()); // e
-console.log(userName.substr(0,1).toLowerCase()); // e
-console.log(userName[userName.indexOf('E')].toLowerCase()); // e
+// let userName = "Elzero";
+// console.log(userName[0].toLowerCase().repeat(3)); // eee
+// console.log(userName.charAt(0).toLowerCase()); // e
+// console.log(userName.slice(0,1).toLowerCase()); // e
+// console.log(userName.substring(0,1).toLowerCase()); // e
+// console.log(userName.substr(0,1).toLowerCase()); // e
+// console.log(userName[userName.indexOf('E')].toLowerCase()); // e
+
+
+//P2
+let word = "Elzero";
+let letterZ = "z";
+let letterE = "e";
+let letterO = "O";
+
+console.log(word.includes(letterZ)); // True inclueds Z
+console.log(word.startsWith(letterE.toUpperCase())); // True starts with E
+console.log(word.endsWith(letterO.toLowerCase())); // True ends with O

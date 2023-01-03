b8c7fef4bcfa44e497d3906c2827b70a7ddd9cf0
Safei Ashraf <safei.ashraf@gmail.com>
2023-01-03 06:27:21 +0200

    feat: solving Numbers p1

--- main.js
+++ main.js
@@ -88,13 +88,20 @@
 // console.log(5e4 + 5e4); // 100000
 
 // Your Solutions
-console.log(1e5); // 100000
-console.log(1_00_000); // 100000
-console.log(100000); // 100000
-console.log(parseInt("100000")); // 100000
-console.log(Number(100000.00)); // 100000
-console.log(10 *1e4); // 100000
-console.log(1_00*1_000); // 100000
-console.log(Math.pow(10,5)); // 100000
-console.log(Math.max(100000,10000,100,1)); // 100000
-console.log(Math.abs(-1e5)); // 100000
+// console.log(1e5); // 100000
+// console.log(1_00_000); // 100000
+// console.log(100000); // 100000
+// console.log(parseInt("100000")); // 100000
+// console.log(Number(100000.00)); // 100000
+// console.log(10 *1e4); // 100000
+// console.log(1_00*1_000); // 100000
+// console.log(Math.pow(10,5)); // 100000
+// console.log(Math.max(100000,10000,100,1)); // 100000
+// console.log(Math.abs(-1e5)); // 100000
+
+//P2
+console.log(Number.MIN_SAFE_INTEGER); // 9007199254740991
+
+//P3
+
+console.log(Number.MAX_SAFE_INTEGER); // 16

b86c0116c76488e03aa1b5f5400d1b2b716e5120
Safei Ashraf <safei.ashraf@gmail.com>
2023-01-04 17:20:17 +0200

    feat: solving DOM exe 2

--- dom.js
+++ dom.js
@@ -6,3 +6,16 @@ imgs.forEach(
 	(img) =>
 		(img.src = "https://elzero.org/wp-content/themes/elzero/imgs/logo.png")
 );
+
+//P2:
+let form = document.querySelector("form");
+form.onsubmit = (e) => {
+	e.preventDefault();
+};
+let usdInput = document.querySelector('[name="dollar"]');
+usdInput.addEventListener("input", (e) => {
+	const ouput = document.querySelector(".result");
+	ouput.textContent = `{${e.target.value}} USD Dollar = {${(
+		Number(e.target.value) * 25.6
+	).toFixed(2)}} Egyptian Pound`;
+});
